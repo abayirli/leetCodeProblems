220a39edeaadf875386647ef3b8a557b05c9b7d1
arifb <arifbayirli@gmail.com>
2020-06-10 23:14:15 +0200

    10th June 2020 added

--- Challange_2020_June/10_SearchInsertPosition.py
+++ Challange_2020_June/10_SearchInsertPosition.py
@@ -1,7 +1,9 @@
 # LeedCode June Challange Day 10
 # Search Insert Position Problem
 
-# Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+# Given a sorted array and a target value, return the index
+# if the target is found. If not, return the index 
+# where it would be if it were inserted in order.
 # You may assume no duplicates in the array.
 
 # Example 1:
